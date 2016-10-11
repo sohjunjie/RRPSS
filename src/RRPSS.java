fb37a12d6bd849951104bf460e662d8dfa84ac26
Sean <Sean@SeanPC>
2016-10-12 02:27:53 +0800

    hi

--- src/RRPSS.java
+++ src/RRPSS.java
@@ -2,8 +2,18 @@
 public class RRPSS {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Staff s = new Staff("John", 1, true, "Chef");
+		System.out.println(s.getName());
+		System.out.println(s.getEmpId());
+		System.out.println(s.getGender());
+		System.out.println(s.getJobTitle());
+		
+		Table t = new Table (1,10);
+		System.out.println(t.getTableId());
+		System.out.println(t.getCapacity());
+		System.out.println(t.getStatus());
+		t.setStatus(Table.KindofStatus.reserved);
+		System.out.println(t.getStatus());
 	}
 
 }
---
+++ src/Staff.java
@@ -0,0 +1,49 @@
+
+public class Staff {
+	private String name;
+	private int emp_id;
+	private boolean genderIsMale;
+	private String job_title;
+	
+	public Staff(String name, int emp_id, boolean genderIsMale, String job_title) {
+		this.name=name;
+		this.emp_id=emp_id;
+		this.genderIsMale=genderIsMale;
+		this.job_title=job_title;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getEmpId () {
+		return emp_id;
+	}
+	
+	public String getGender() {
+		if (genderIsMale==true)
+			return "Male";
+		else
+			return "Female";
+	}
+	
+	public String getJobTitle() {
+		return job_title;
+	}
+	
+	public void setName(String name) {
+		this.name=name;
+	}
+	
+	public void setEmpId(int emp_id) {
+		this.emp_id=emp_id;
+	}
+	
+	public void setGender(boolean genderIsMale) {
+		this.genderIsMale=genderIsMale;
+	}
+	
+	public void setJobTitle(String job_title) {
+		this.job_title=job_title;
+	}
+}
