9ce206aff2943d03dd7db9b00def3a8c067fad4d
sohjunjie <junjie.soh93@gmail.com>
2016-10-15 11:55:34 +0800

    add price to menuitem

--- src/classes/MenuItem.java
+++ src/classes/MenuItem.java
@@ -4,16 +4,20 @@ public abstract class MenuItem {
 	
 	private String 		menuName;
 	private String 		desc;
+	private double		price;
 	
-	public MenuItem(String menuName, String desc){
+	public MenuItem(String menuName, String desc, double price){
 		this.menuName = menuName;
 		this.desc = desc;
+		this.price = price;
 	}
 	
 	public void setMenuName(String menuName){ this.menuName = menuName; }
 	public void setDesc(String desc){ this.desc = desc; }
+	public void setPrice(double price){ this.price = price; }
 	
 	public String getMenuName(){ return this.menuName; }
 	public String getDesc(){ return this.desc; }
+	public double getPrice(){ return this.price; }
 
 }
