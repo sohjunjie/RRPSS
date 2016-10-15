285db14fee38a306b31ac7a699141b2310b5b8c8
sohjunjie <junjie.soh93@gmail.com>
2016-10-15 23:59:09 +0800

    improve readability of PromotionPackage.java

--- src/classes/PromotionPackage.java
+++ src/classes/PromotionPackage.java
@@ -4,42 +4,45 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PromotionPackage extends MenuItem {
-	public ArrayList<Food> foodSet;
+
+	private ArrayList<Food> foodSet;
 	private int index;
-	private Scanner sc = new Scanner(System.in);
 	
 	public PromotionPackage(String menuName, String desc, double price) {
 		super(menuName, desc, price);
 		this.foodSet = new ArrayList<Food>();
-	}
-	
+	}	
 	public PromotionPackage(String menuName, String desc, double price, ArrayList<Food> foodSet) {
 		super(menuName, desc, price);
 		this.foodSet = foodSet;
-		
 	}
+
+	public ArrayList<Food> getFoodSet(){ return foodSet; }	
+	public void setFoodSet(ArrayList<Food> foodSet){ this.foodSet = foodSet; }
 	
-	public void addFood(Food food) {
-		foodSet.add(food);
-	}
+	public void addFood(Food food){ foodSet.add(food); }
 	
 	public void removeFood() {
+		
+		Scanner sc = new Scanner(System.in);
+		
 		System.out.println("What food would you like to remove from the Promotion Package?");
-		for (Food s : foodSet)
-			System.out.println(foodSet.indexOf(s) + ": " + s.getMenuName());
+		
+		for (Food fd : foodSet)
+			System.out.println(foodSet.indexOf(fd) + ": " + fd.getMenuName());
+
 		index = sc.nextInt();
-		if (index > -1) {
+		sc.close();
+		
+		try {
 			String foodNameRemoved = foodSet.get(index).getMenuName();
 			foodSet.remove(index);
 			System.out.println(foodNameRemoved + " removed from promotion package."); 
+		}catch(IndexOutOfBoundsException e){
+			System.out.println("Food item removal failed! (Invalid index provided");
 		}
+		
 	}
 	
-	public ArrayList<Food> getFoodSet() {
-		return foodSet;
-	}
-	
-	public void setFoodSet(ArrayList<Food> foodSet) { 
-		this.foodSet = foodSet;
-	}
+
 }
