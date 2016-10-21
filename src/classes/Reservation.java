25e93cac6a33db9b0f28168547da9ebc3082817a
sohjunjie <junjie.soh93@gmail.com>
2016-10-21 17:29:32 +0800

    made reservation class readable

--- src/classes/Reservation.java
+++ src/classes/Reservation.java
@@ -6,36 +6,36 @@ import java.util.Calendar;
 public class Reservation implements Serializable{
 	
 	private static final long serialVersionUID = 3558136592677236481L;
-	private int reservationID;
-	private int numPax;
-	private String customerName;				//only customerName, customerContact and  acceptStatus can be changed
-	private int customerContact;
-	private boolean acceptedStatus;				//true = accepted, false = not yet accepted
-	private Calendar arrivalTime;		//reservationTime and numPax cannot be changed, create new reservation is necessary
+	private int 		reservationID;
+	private int 		numPax;
+	private String 		customerName;
+	private int 		customerContact;
+	private boolean 	accepted;
+	private Calendar 	arrivalTime;
 	
 	public Reservation(String customerName, int customerContact, int numPax, int reservationID, Calendar arrivalTime){
 		this.customerName = customerName;
 		this.customerContact = customerContact;
 		this.numPax = numPax;
-		this.acceptedStatus = false;
+		this.accepted = false;
 		this.reservationID = reservationID;
 		this.arrivalTime = arrivalTime;
 	}
-	
-	public void acceptReservation(){ 
-		this.acceptedStatus = true;
+
+	public void acceptReservation(){
+		this.accepted = true;
 		Restaurant.reservations.remove(this);
 		Restaurant.settledReservations.add(this);
 	}
-	
-	public boolean getAcceptedStatus(){ return this.acceptedStatus; }
+
+	public boolean getAcceptedStatus(){ return this.accepted; }
 	public String getCustomerName(){ return this.customerName; }
 	public int getCustomerContact(){ return this.customerContact; }
 	public int getNumPax(){ return this.numPax; }
-	public int getReservationID(){ return this.reservationID; }	
+	public int getReservationID(){ return this.reservationID; }
 	public Calendar getArrivalTime(){ return this.arrivalTime; }
 	
-	public void setCustomerName(String newName){ this.customerName = newName; }	
+	public void setCustomerName(String newName){ this.customerName = newName; }
 	public void setCustomerContact(int newContact) { this.customerContact = newContact; }
 
 }
