4c1c97aa6e6d132aa52228423e0299cd00705cef
Sean <Sean@10.27.154.233>
2016-10-28 14:18:02 +0800

    javadoc

--- src/ui/OrderUI.java
+++ src/ui/OrderUI.java
@@ -5,9 +5,22 @@ import java.util.Scanner;
 import classes.Order;
 import db.Restaurant;
 
+/**
+ * Represents the UI displayed to the user when creating, changing or finalising orders.
+ * @author Sean
+ *
+ */
+
 public class OrderUI {
 
 	private static Scanner sc = new Scanner(System.in);
+	
+
+	/**
+	 * While loop that scans user input to decide which operation to execute.
+	 * Can create order, remove order line item from order or create invoice.
+	 * Terminate by entering number greater than 4.
+	 */
 	
 	public static void menuShowOrderOptions(Order order){
 		
@@ -40,13 +53,31 @@ public class OrderUI {
         
 	}
 	
+	/**
+	 * Adds order line item to order
+	 * @param order Order that is added to
+	 */
+	
 	public static void takeOrder(Order order){
 		order.addOrderItem();
 	}
 	
+	/**
+	 * Remove order line item from order
+	 * @param order Order that is removed from
+	 */
+	
 	public static void removeOrderItem(Order order){
 		order.removeOrderItem();
 	}
+	
+	/**
+	 * Generates invoice for the order if it does not exist.
+	 * Adds the invoice to the restaurant's ArrayList of all of its invoices.
+	 * Prints the invoice showing date and time, invoice number, all items ordered,
+	 * price of the items, GST applied and total price to be paid.
+	 * @param order Order that invoice is generated for
+	 */
 	
 	public static void printInvoice(Order order){
 		if(order.getInvoice() == null){
