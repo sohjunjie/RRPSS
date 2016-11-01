e4dda40be4a14a8d6ca83179bfec3b35c1af682f
sohjunjie <junjie.soh93@gmail.com>
2016-11-01 21:53:16 +0800

    segregate ui and logic further

--- src/mgr/FoodMenuMgr.java
+++ src/mgr/FoodMenuMgr.java
@@ -16,84 +16,41 @@ import db.Restaurant;
  */
 public class FoodMenuMgr {
 
-	private static ArrayList<MenuItem> foodMenu = Restaurant.foodMenu;	
+	private static ArrayList<MenuItem> foodMenu = Restaurant.foodMenu;
 	private static Scanner sc = new Scanner(System.in);
+	
+	public static ArrayList<MenuItem> getRestaurantFoodMenu(){ return foodMenu; }
 	
 	/**
 	 * Create new food item into the restaurant food menu.
-	 * User will be prompt for its menu name, description,
-	 * price and course type.
+	 * @param foodName Name of the food
+	 * @param foodDesc Description of the food
+	 * @param foodPrice Price of the food
+	 * @param foodCourseType Course type of the food
 	 */
-	public static void createNewFood(){
-		// TODO: Create new food and add to food menu
-		String foodName;
-		String foodDesc;
-		double foodPrice;
-		Food.CourseType foodCourseType = null;
-		
+	public static void createNewFood(String foodName, String foodDesc, double foodPrice, Food.CourseType foodCourseType){		
 		Food newFood;
-		
-		System.out.print("Enter menu name of food: "); foodName = sc.nextLine();
-		System.out.print("Enter menu description of food: "); foodDesc = sc.nextLine();
-		System.out.print("Enter price of food: "); foodPrice = sc.nextDouble();
-		
-		int choice;
-		do{
-			System.out.println("\nSelect course type of your food.");
-			int index = 1;
-			for (Food.CourseType courseType : Food.CourseType.values())
-				System.out.println((index++) + " " + courseType);
-	    	System.out.print("    Enter the number of your choice: ");
-	    	choice = sc.nextInt();
-	    	switch (choice) {
-		        case 1:
-		        	foodCourseType = Food.CourseType.MAIN_COURSE;
-		            break;
-		        case 2:
-		        	foodCourseType = Food.CourseType.DRINKS;
-		            break;
-		        case 3:
-		        	foodCourseType = Food.CourseType.DESSERT;
-		            break;
-		        default:
-		        	System.out.println("Invalid choice entered!");
-		            break;
-	        }
-	    
-		}while(choice > 3 || choice < 0);
-		
 		newFood = new Food(foodName, foodDesc, foodPrice, foodCourseType);
 		foodMenu.add((MenuItem) newFood);
 		System.out.println("Food " + foodName + " was added to the food menu.");
-		
 	}
 	
 	/**
-	 * Create a promotion package menu item and add to
-	 * the restaurant food menu. User will be prompt for
-	 * its name, description, price and the food to add
-	 * to promotion package.
+	 * Create a promotion package into the restaurant food menu
+	 * @param menuName Name of the promotion package
+	 * @param menuDesc Description of the promotion package
+	 * @param menuPrice Price of the promotion package
 	 */
-	public static void createNewPromotionPackage(){
+	public static void createNewPromotionPackage(String menuName, String menuDesc, double menuPrice){
 
-		String menuName;
-		String menuDesc;
-		double menuPrice;
-		PromotionPackage newPP;
-		
-		System.out.print("Enter menu name of promotion package: "); menuName = sc.nextLine();
-		System.out.print("Enter menu description of promotion package: "); menuDesc = sc.nextLine();
-		System.out.print("Enter price of promotion package: "); menuPrice = sc.nextDouble();
-		
+		PromotionPackage newPP;		
 		newPP = new PromotionPackage(menuName, menuDesc, menuPrice);
 		
 		boolean continueAdd = true;
 		do{
-			
 			newPP.addFood(foodMenu);
-			
 			System.out.println("Continue adding menu item to promotion package? (Y/N)");
-			continueAdd = ('Y' == Character.toUpperCase(sc.next().charAt(0)));			
+			continueAdd = ('Y' == Character.toUpperCase(sc.next().charAt(0)));
 		}while(continueAdd);
 		
 		foodMenu.add((MenuItem) newPP);
@@ -101,27 +58,16 @@ public class FoodMenuMgr {
 	}
 	
 	/**
-	 * Remove a menu item from the restaurant food menu.
-	 * User will be prompt to select the menu item to
-	 * remove.
+	 * Remove a menu item from the restaurant food menu
+	 * base on a menu index
+	 * @param menuIndex menu item to remove
 	 */
-	public static void removeMenuItem(){
+	public static void removeMenuItem(int menuIndex){
 		
-		int choice;
 		String itemRemoved;
-		
-		int index = 0;
-		System.out.println("\nSelect the menu item to remove from the food menu:");
-		for(MenuItem menuItem : foodMenu)
-			System.out.println("(" + index++ + ") " + menuItem.getMenuName());
-
-    	System.out.print("    Enter the number of your choice: ");
-		choice = sc.nextInt();
-		sc.close();
-		
 		try {
-			itemRemoved = foodMenu.get(choice).getMenuName();
-			foodMenu.remove(choice);
+			itemRemoved = foodMenu.get(menuIndex).getMenuName();
+			foodMenu.remove(menuIndex);
 			System.out.println(itemRemoved + " removed from food menu."); 
 		}catch(IndexOutOfBoundsException e){
 			System.out.println("Remove menu item from food menu failed! (Invalid index provided");
--- src/ui/FoodMenuUI.java
+++ src/ui/FoodMenuUI.java
@@ -1,7 +1,10 @@
 package ui;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
+import classes.Food;
+import classes.MenuItem;
 import mgr.FoodMenuMgr;
 
 /**
@@ -36,19 +39,105 @@ public class FoodMenuUI {
             
             switch (choice) {
                 case 1: 
-                		FoodMenuMgr.createNewFood();
+                		createNewFoodUI();
                         break;
                 case 2:
-                		FoodMenuMgr.createNewPromotionPackage();
+                		createNewPromotionPackageUI();
                         break;
                 case 3:
-                		FoodMenuMgr.removeMenuItem();
+                		removeMenuItemUI();
                     	break;
                 case 4:
             }
 
         } while (choice < 4);
 		
+	}
+	
+	/**
+	 * Show a UI that prompts user for menu item to be
+	 * removed.
+	 */
+	public static void removeMenuItemUI(){
+		
+		int choice;
+		ArrayList<MenuItem> foodMenu = FoodMenuMgr.getRestaurantFoodMenu();
+
+		int index = 0;
+		System.out.println("\nSelect the menu item to remove from the food menu:");
+		for(MenuItem menuItem : foodMenu)
+			System.out.println("(" + index++ + ") " + menuItem.getMenuName());
+
+    	System.out.print("    Enter the number of your choice: ");
+		choice = sc.nextInt();
+
+		FoodMenuMgr.removeMenuItem(choice);
+
+	}
+	
+	/**
+	 * Show a UI that prompts user for details of
+	 * the promotion package to be created.
+	 */
+	public static void createNewPromotionPackageUI(){
+		
+		String menuName;
+		String menuDesc;
+		double menuPrice;
+		
+		System.out.print("Enter menu name of promotion package: "); menuName = sc.nextLine();
+		System.out.print("Enter menu description of promotion package: "); menuDesc = sc.nextLine();
+		System.out.print("Enter price of promotion package: "); menuPrice = sc.nextDouble();
+		
+		FoodMenuMgr.createNewPromotionPackage(menuName, menuDesc, menuPrice);
+	}
+	
+	public static void getFoodUI(ArrayList<MenuItem> foodMenu){
+		
+	}
+	
+	/**
+	 * Show a UI that prompts user for details of
+	 * the food to be created.
+	 */
+	public static void createNewFoodUI(){
+		
+		String foodName;
+		String foodDesc;
+		double foodPrice;
+		Food.CourseType foodCourseType = null;
+
+		System.out.print("Enter menu name of food: "); foodName = sc.nextLine();
+		System.out.print("Enter menu description of food: "); foodDesc = sc.nextLine();
+		System.out.print("Enter price of food: "); foodPrice = sc.nextDouble();
+		
+		int choice;
+		do{
+			System.out.println("\nSelect course type of your food.");
+			int index = 1;
+			for (Food.CourseType courseType : Food.CourseType.values())
+				System.out.println((index++) + " " + courseType);
+	    	System.out.print("    Enter the number of your choice: ");
+	    	choice = sc.nextInt();
+	    	switch (choice) {
+		        case 1:
+		        	foodCourseType = Food.CourseType.MAIN_COURSE;
+		            break;
+		        case 2:
+		        	foodCourseType = Food.CourseType.DRINKS;
+		            break;
+		        case 3:
+		        	foodCourseType = Food.CourseType.DESSERT;
+		            break;
+		        default:
+		        	System.out.println("Invalid choice entered!");
+		            break;
+	        }
+	    
+		}while(choice > 3 || choice < 0);
+		
+		FoodMenuMgr.createNewFood(foodName, foodDesc, foodPrice, foodCourseType);
+
 	}
 	
 }
